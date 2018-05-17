2fa83c9c274b1bf0593f2d1cd33c07ad30a09ffe
tencypriya <36623605+tencypriya@users.noreply.github.com>
2018-05-17 10:08:15 +0530

    Update positive.py

--- beginner level/positive.py
+++ beginner level/positive.py
@@ -1,8 +1,8 @@
-num = float(input("Enter a number: "))
-if num >= 0:
-   if num == 0:
-       print("Zero")
+num=float(input("enter the no"))
+if num>=0
+  if num==0:
+       print("zero")
    else:
-       print("Positive number")
-else:
-   print("Negative number")
+       print("positive no")
+         else:
+            print("negative no")
