7dbf5e80713eb30a731c96c4e54c2c2c20c2a681
tencypriya <36623605+tencypriya@users.noreply.github.com>
2018-05-23 18:21:28 +0530

    Create prime not.py

---
+++ beginner level/prime not.py
@@ -0,0 +1,10 @@
+n=input(int("enter the num"))
+if(n>1):
+for i in range(2,n):
+if(n%i)==0
+print(n,"is not a prime number")
+break
+else:
+print(n,"prime number")
+else:
+print(n,"not a prime number")
